493d756323eabd864b410d08157e6fd15ec90ba6
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2020-08-17 18:23:27 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for x in range(len(arr)):
+        if arr[x] == target:
+            return x
 
     return -1   # not found
 
@@ -9,6 +11,20 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] < target:
+            left = mid + 1
+
+        else:
+            right = mid - 1
 
 
     return -1  # not found
